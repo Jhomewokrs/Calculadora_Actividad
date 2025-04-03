af37bb9acc55145220f4397371774fa6f87e57c4
Jhomeworks <jovhomeworks@gmail.com>
2025-04-02 21:09:39 -0300

    calculadora

---
+++ cal.py
@@ -0,0 +1,49 @@
+while True: 
+       
+ variable1= int(input('ingrese un numero'))
+ print(" ")
+ variable2= int(input('ingrese otro numero'))
+
+ print(" ")
+ print ("+.Suma")
+ print ("-.Resta")
+ print ("*.Multiplicacion")
+ print ("/.Division")
+ print("")
+ print ("eliga el tipo de operacion que desea realizar:")
+ eleccion= (input())
+
+ if eleccion == '+':
+        print(" ")
+        print("Suma")
+        print(f"resultado de sumas: {variable1+variable2}" )
+
+ elif eleccion == '-':
+        print(" ")
+        print("Resta")
+        print(f"resultado de restas: {variable1-variable2}")
+
+ elif eleccion == '*':
+        print(" ")
+        print("Multiplicacion")
+        print(f"resultado de multiplicacion: {variable1*variable2}")
+        
+ elif eleccion == '/':
+        print(" ")
+        print("Division")
+        print(f"resultado de division: {variable1/variable2}") 
+ else:
+        print("te equivocastes de opcion")
+
+
+ 
+ op = input("¿Quieres continuar? (si/no): ")
+
+ if  op == "si":
+        print("Ok, continuemos.")
+ elif op == "no":
+        print("Adiós.")
+ else:
+        print("Error: Respuesta no válida. Por favor, ingresa 'si' o 'no'.")
+        break
+ 
